2ecbbca4fe2e7df82a0795cc1c6d9e71bf36cc28
wellington7 <wellington.araujo.silva@ccc.ufcg.edu.br>
2014-08-19 11:59:25 -0300

    uploading

--- app/models/Local.java
+++ app/models/Local.java
@@ -1,22 +1,33 @@
 package models;
 
+import models.exceptions.EventoInvalidoException;
+import models.exceptions.LocalInvalidoException;
+
 public class Local {
 
 	private String nome;
 	private int capacidade;
 	private String rota;
 	
-	public Local(String nome, int capacidade, String rota){
-		this.nome = nome;
-		this.capacidade = capacidade;
-		this.rota = rota;
+	public Local(){
+		
+	}
+	
+	public Local(String nome, int capacidade, String rota) throws LocalInvalidoException{
+		setNome(nome);
+		setCapacidade(capacidade);
+		setRota(rota);
 	}
 		
 	public String getNome() {
 		return nome;
 	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome) throws LocalInvalidoException {
+		if (nome == null)
+			throw new LocalInvalidoException("O parâmetro nome está nulo");
+		if (nome.length() > 40)
+			throw new LocalInvalidoException("O parâmetro nome está longo");
 		this.nome = nome;
 	}
 
@@ -24,7 +35,11 @@ public class Local {
 		return capacidade;
 	}
 
-	public void setCapacidade(int capacidade) {
+	public void setCapacidade(int capacidade) throws LocalInvalidoException{
+		if (capacidade == 0)
+			throw new LocalInvalidoException("O parâmetro capacidade está zerado");
+		if (capacidade > 9999999)
+			throw new LocalInvalidoException("Capacidade do local super estimada");
 		this.capacidade = capacidade;
 	}
 
@@ -32,7 +47,11 @@ public class Local {
 		return rota;
 	}
 
-	public void setRota(String rota) {
+	public void setRota(String rota) throws LocalInvalidoException{
+		if (rota == null)
+			throw new LocalInvalidoException("O parâmetro rota está nulo");
+		if (rota.length() > 450)
+			throw new LocalInvalidoException("O parâmetro rota está muito longo");
 		this.rota = rota;
 	}
 	
---
+++ app/models/exceptions/LocalInvalidoException.java
@@ -0,0 +1,9 @@
+package models.exceptions;
+
+public class LocalInvalidoException extends Exception{
+	
+	public LocalInvalidoException(String mensagem){
+		super(mensagem);
+	}
+
+}
